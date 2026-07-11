8d20065ab5ec7f373659fbf830e064f3e1a30379
GOMEZANGELICAMARIE <angelicamariegomez1@gmail.com>
2026-07-11 12:33:09 +0800

    get random meals

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetRandomMeal.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetRandomMeal.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 1: GET Random Meal
@@ -5,7 +9,7 @@ namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 //
 // Your task:
 // 1. Use the HttpClient to send a GET request to the URL above
- // 2. Read the response as a string
+// 2. Read the response as a string
 // 3. Assert that the status code is 200 OK
 // 4. Assert that the response body is not null or empty
 //
@@ -16,11 +20,24 @@ public static class GetRandomMeal
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/random.php
-        // TODO: Read the response content as a string
-        // TODO: Assert status code is 200 OK
-        // TODO: Assert response body is not null or empty
+        string url = "https://themealdb.com/api/json/v1/1/random.php";
 
-        throw new NotImplementedException();
+        // 1. Use the HttpClient to send a GET request to the URL above
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        // 3. Assert that the status code is 200 OK
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new Exception($"Assertion failed: Status code was {response.StatusCode}, expected 200 OK.");
+        }
+
+        // 2. Read the response as a string
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        // 4. Assert that the response body is not null or empty
+        if (string.IsNullOrEmpty(responseBody))
+        {
+            throw new Exception("Assertion failed: Response body is null or empty.");
+        }
     }
 }
